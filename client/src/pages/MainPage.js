0291f6d388587839814994558f0c47ab467ced40
Kanishka99 <akcadikari@sttd.appsc.sab.ac.lk>
2024-05-19 15:22:26 +0530

    ubdate clientpage

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -3,12 +3,44 @@ import React from 'react'
 export default function MainPage () {
   return (
     <div>
-        <h1 className='text-4xl font-bold text-green-500'>Convert Your Currencies Today</h1>
-        <p>Melcome to "Convert Your Currencies Today" This application allows
+        <h1 className='text-4xl font-bold text-green-500 '>Convert Your Currencies Today</h1>
+        <p className='lg:mx-32 opacity-40 py-5'>Melcome to "Convert Your Currencies Today" This application allows
          you to easily convert currencies based on the latest exchange rates.
          Whether you're planning a trip, managing your finances,
          er simply curious about the value of your money in different currencies, 
          this teel is here to help.</p>
+   
+       
+    <div className='mt-5 flex items-center justify-center flex-col'>   
+       <section className='w-full lg:w-1/2'>
+        <form>
+            <div c.text-gray-900{
+        <lab  --tw-text-opacity:1;
+        ht color:rgb(17 24 39/var(--tw-text-opacity));
+        cl}
+        text-gray-900 dark:text-white"
+        
+        >
+    
+   Data
+    </label>
+
+    
+    <input type="Date" 
+    id=""
+     className="bg-gray-50 border border-gray-300 text-gray-900
+      text-sm rounded-lg focus:ring-gray-500 
+      focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 
+      dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
+      dark:focus:ring-blue-500 dark:focus:border-gray-500" placeholder="name@flowbite.com" required 
+      placeholder="name@flowbite.com"
+      required
+      />
+  </div>
+ 
+</form>
+</section>
     </div>
+    
   )
 }
