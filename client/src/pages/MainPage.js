acf0c705df3c8e45c74949cc6eda74b228c7d1ee
Kanishka99 <akcadikari@sttd.appsc.sab.ac.lk>
2024-08-31 03:08:35 +0530

    update02

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react';
 
 export default function MainPage () {
+const [Nowdate , setNowDate]= useState(new Date());
+const [SourceCurrency , setSourceCurrency]= useState('GBP');
+const [AmountInCurrency , setAmountInCurrency]= useState("USD");
+const [TargetCurrency , setTargetCurrency]= useState(0);
+
+const handleSubmit=(e)=>{};
+
+
+
   return (
     <div>
         <h1 className='text-4xl font-bold text-yellow-500 border '>Let's explore currency converson simply</h1>
@@ -12,11 +21,11 @@ export default function MainPage () {
        
         <div className='mt-5 flex items-center justify-center flex-col'>   
             <section className='w-full lg:w-1/2'>
-                <form>
+                <form onSubmit={handleSubmit}>
                 <div className='mb-4'>
 
 <label  
-    htmlFor=''
+    htmlFor={Nowdate}
     className='block mb-2 text-sm font-mediumtext-yellow-900 dark:text-white'>
 
         Now Date
@@ -25,7 +34,9 @@ export default function MainPage () {
 
 <input 
     type="Date" 
-    id=""
+    onChange={(e)=>setNowDate(e.target.value)}
+    id={Nowdate}
+    name={Nowdate}
     className="bg-yellow-50 border border-yellow-300 text-yellow-900
     text-sm rounded-lg focus:ring-yellow-500 
     focus:border-yellow-500 block w-full p-2.5 dark:bg-yellow-700 
@@ -38,12 +49,14 @@ export default function MainPage () {
 </div>
 <div className='mb-4'>
                                 <label  
-                                    htmlFor=''
+                                    htmlFor={SourceCurrency}
+
                                     className='block mb-2 text-sm font-medium text-yellow-900 dark:text-white'
                                 >
                                     Source Currency
                                 </label>
-                                <select className="bg-yellow-50 border border-yellow-300 text-yellow-900
+                                <select onChange={(e)=>setSourceCurrency(e.target.value)}
+                                 className="bg-yellow-50 border border-yellow-300 text-yellow-900
                                         text-sm rounded-lg focus:ring-yellow-500 
                                         focus:border-yellow-500 block w-full p-2.5 dark:bg-yellow-700 
                                         dark:border-yellow-600 dark:placeholder-yellow-400 dark:text-white 
@@ -51,8 +64,9 @@ export default function MainPage () {
                                         placeholder="SourceCurrency"  
                                         required
                                         
-                                        name=''
-                                        id=''>
+                                        name={SourceCurrency}
+                                        id={SourceCurrency}
+                                        value={SourceCurrency}>
                                             <option value=''>Sellect Source currency</option>
 
 
@@ -62,12 +76,13 @@ export default function MainPage () {
 
  <div className='mb-4'>
                                 <label  
-                                    htmlFor=''
+                                    htmlFor={TargetCurrency}
                                     className='block mb-2 text-sm font-medium text-white-900 dark:text-White'
                                 >
                                     Target Currency
                                 </label>
-                                <select className="bg-yellow-50 border border-yellow-300 text-yellow-900
+                                <select onChange={(e)=>setTargetCurrency(e.target.value)}
+                                className="bg-yellow-50 border border-yellow-300 text-yellow-900
                                         text-sm rounded-lg focus:ring-yellow-500 
                                         focus:border-yellow-500 block w-full p-2.5 dark:bg-yellow-700 
                                         dark:border-yellow-600 dark:placeholder-yellow-400 dark:text-white 
@@ -75,8 +90,10 @@ export default function MainPage () {
                                         placeholder=" Target Currency"  
                                         required
                                         
-                                        name=''
-                                        id=''>
+                                        name={TargetCurrency}
+                                        id={TargetCurrency}
+                                        value={TargetCurrency}
+                                         >
                                             <option value=''>Sellect  Target Currency</option>
 
 
@@ -86,26 +103,33 @@ export default function MainPage () {
                             <div className='mb-4'>
 
 <label  
-    htmlFor=''
-    className='block mb-2 text-sm font-mediumtext-yellow-900 dark:text-white'>
+    htmlFor={AmountInCurrency}
+    className='block mb-2 text-sm font-mediumtext-white-900 dark:text-white'>
 
-        Amount Currency
+        Amount In Currency
 </label>
 
 
 <input 
+onChange={(e)=>setAmountInCurrency(e.target.value)}
     type="text" 
-    id=""
+    id={AmountInCurrency}
     className="bg-yellow-50 border border-yellow-300 text-yellow-900
     text-sm rounded-lg focus:ring-yellow-500 
     focus:border-yellow-500 block w-full p-2.5 dark:bg-yellow-700 
-    dark:border-yellow-600 dark:placeholder-yellow-400 dark:text-white 
+    dark:border-yellow-600 dark:placeholder-white-400 dark:text-black 
     dark:focus:ring-yellow-500 dark:focus:border-yellow-500" 
     placeholder="Enter Your Currency Amount"  
     required
-/>
+    name={AmountInCurrency}
+    
+    />
 
 </div>
+
+<button className='bg-green-600 hover:bg-green-700 
+text-white font-medium py-2 px-4 rounded-md'>
+    Give the currency</button>
 
                     
                 </form>
